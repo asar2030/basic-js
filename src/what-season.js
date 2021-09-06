d28701f996aab416c00c1f4391cee93f3e7c5969
Asar Akhmetbekov <asarakhmetbekov@MacBook-Pro-Asar.local>
2021-09-06 22:24:33 +0600

    feat: add one more

--- src/what-season.js
+++ src/what-season.js
@@ -11,7 +11,19 @@ import { NotImplementedError } from '../extensions/index.js';
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-export default function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getSeason(date) {
+  if (date === undefined) return 'Unable to determine the time of year!'
+
+
+  let month = date.getMonth() + 1
+
+  if (month == 1 || month == 2 || month == 12) {
+      return 'winter'
+  } else if (month >= 3 && month < 6) {
+      return 'spring'
+  } else if (month >= 6 && month < 9) {
+      return 'summer'
+  } else if (month >= 9 && month < 12) {
+      return 'automn'
+  }
 }
